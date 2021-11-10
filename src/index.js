b98e19b126f23aa6d2f45b18c0d2eae80b65d4c7
hyunkim <94027960+hyunthekim@users.noreply.github.com>
2021-11-10 17:24:17 +0900

    update 연산자내용추가

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-let a = 8;
+let a = 1;
 a--;
-console.log(a); //산술연산자, -,+,*,/
+console.log(a); //산술연산자
+
+const b = !true;
+console.log(b); //논리연산, not연산자
+
+const c = !false;
+console.log(c); //not연산자
